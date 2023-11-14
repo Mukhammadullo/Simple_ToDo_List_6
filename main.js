7727946c07d7f10d690f4a72f7c011466423fe2e
Muhammadullo <mnastulloev@gmail.com>
2023-11-14 06:06:40 +0500

    get_data-in_js

---
+++ main.js
@@ -0,0 +1,18 @@
+let tbody = document.querySelector(".tbody")
+
+// data
+let data = [
+    { id: 1, text: "welcome to App TODO" },
+    { id: 2, text: "Сделать уборку" },
+    { id: 3, text: "Отвести братика в садик" },
+    { id: 4, text: "Сходить на дополнительные уроки" },
+    { id: 5, text: "Сделать уроки" },
+    { id: 6, text: "Заниматься спортом" },
+    { id: 7, text: "Сходит в магазин" },
+]
+
+
+// get
+function get() {
+
+}
